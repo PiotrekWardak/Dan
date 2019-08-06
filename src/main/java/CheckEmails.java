62791012f104b4bb8ad144d35d47cb5e883c84b9
PiotrWardak <piotrekwardak@gmail.com>
2019-08-06 20:34:17 +0200

    Poczatek pracy zaczytywaniem emaili

---
+++ src/main/java/CheckEmails.java
@@ -0,0 +1,26 @@
+import javax.mail.Session;
+
+public class CheckEmails {
+
+
+    public CheckEmails(Session session)
+    {
+
+
+//        Store store = null;
+//        try {
+//            store = session.getStore("smtp");
+//        } catch (NoSuchProviderException e) {
+//            e.printStackTrace();
+//        }
+//
+//        store.connect(host, user, password);
+
+
+    }
+
+
+
+
+
+}
